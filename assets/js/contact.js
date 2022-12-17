2f1ec7ede02afdc8c1485e455af70c85b265418e
DrilonSalihu <119863861+DrilonSalihu@users.noreply.github.com>
2022-12-17 15:02:46 +0100

    Add files via upload

---
+++ assets/js/contact.js
@@ -0,0 +1,38 @@
+$(function () {
+  $('#contact-form').validator();
+
+  // when the form is submitted
+  $('#contact-form').on('submit', function (e) 
+  {
+  	// if the validator does not prevent form submit
+    if (!e.isDefaultPrevented()) {
+    	var url = "contact.php";
+	
+			// POST the values in the background
+   		$.ajax({
+      	type: "POST",
+        url: url,
+        data: $(this).serialize(),
+        success: function (data)
+        {
+        	// data = JSON object that contact.php returns
+					// we recieve the type of the message: success x danger and apply it to the 
+          var messageAlert = 'alert-' + data.type;
+          var messageText = data.message;
+					
+          var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+          
+					// If we have messageAlert and messageText
+          if (messageAlert && messageText) {
+          	// inject the alert to .messages div in our form
+            $('#contact-form').find('.messages').html(alertBox);
+            // empty the form
+            $('#contact-form')[0].reset();
+          }
+        }
+      });
+    	
+			return false;
+  	}
+  })
+});
